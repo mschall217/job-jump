25f4662703ba04e8f259c21622edd83ad7ffa56b
Brandon <hutchingsonb@gmail.com>
2021-03-06 11:34:22 -0500

    created a function called on submit click to filter api search parameters

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
-const keyword = document.querySelector('#keyword');
-const place = document.querySelector('#place');
+const keywordEl = document.querySelector('#keyword');
+const placeEl = document.querySelector('#place');
 const result = document.querySelector('#result');
+const searchbtn = document.querySelector('.btn');
+
+const keyword = keywordEl.value;
+const place = placeEl.value;
+
+console.log(keyword);
 
 
 
-const jobApiUrl = 'https://data.usajobs.gov/api/Search?Keyword=software&Page=3&ResultsPerPage=50&LocationName=Ohio';
-const homeApiUrl = 'https://api.covidtracking.com/v1/us/current.json';
 
-fetch(jobApiUrl, {
-    headers: {
-        'Authorization-key': 'RN/eTW8teoxznCRynHSZxey6do4S52rVL47RUyd+Y1E='
-    }
-})
-    .then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-            })
+
+function callApi() {
+    //gets input field values
+    const keyword = keywordEl.value;
+    const place = placeEl.value;
+
+    const jobApiUrl = 'https://data.usajobs.gov/api/Search?Keyword=' + keyword + '&ResultsPerPage=50&LocationName=' + place;
+    const homeApiUrl = 'https://api.covidtracking.com/v1/us/current.json';
+    console.log(keyword);
+    console.log(place);
+    console.log(jobApiUrl);
+    fetch(jobApiUrl, {
+        headers: {
+            'Authorization-key': 'RN/eTW8teoxznCRynHSZxey6do4S52rVL47RUyd+Y1E='
         }
     })
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                })
+            }
+        })
+    
+    
+    fetch(homeApiUrl)
+        .then(function (response) {
+            console.log(response);
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                })
+            }
+        })
+}
 
-
-fetch(homeApiUrl)
-    .then(function (response) {
-        console.log(response);
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-            })
-        }
-    })
-
+searchbtn.addEventListener('click', callApi);
